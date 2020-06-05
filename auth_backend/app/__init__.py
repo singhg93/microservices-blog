575bd482dc04139e62018fe596b9e0dec8cc89bd
singhg93 <singhg93@student.douglascollege.ca>
2020-06-04 22:53:31 -0700

    added the routes endpoints

--- auth_backend/app/__init__.py
+++ auth_backend/app/__init__.py
@@ -14,16 +14,30 @@ def create_app(config_name="default"):
     db.init_app(app)
     migrate = Migrate(app, db)
 
+    from .routes import jwt
+    jwt.init_app(app)
+
     try:
         os.makedirs(app.instance_path)
     except OSError:
         pass
 
     from .routes import (
-        GetUser, Register
+        GetUser, Register, Login, FreshLogin,
+        RefreshToken, Logout, ValidateToken,
+        ValidateFreshToken
     )
-    app.add_url_rule('/get/<username>', view_func=GetUser.as_view('hello'))
+    app.add_url_rule('/get', view_func=GetUser.as_view('hello'))
     app.add_url_rule('/register', view_func=Register.as_view('register'))
+    app.add_url_rule('/login', view_func=Login.as_view('login'))
+    app.add_url_rule('/fresh_login', view_func=FreshLogin.as_view('fresh_login'))
+    app.add_url_rule('/refresh', view_func=RefreshToken.as_view('refresh'))
+    app.add_url_rule('/logout', view_func=Logout.as_view('logout'))
+    app.add_url_rule('/validate_token', view_func=ValidateToken.as_view('validate_token'))
+    app.add_url_rule('/validate_fresh_token', view_func=ValidateFreshToken.as_view('validate_fresh_token'))
 
+
+    import tests
+    tests.test_init_app(app)
 
     return app
--- auth_backend/app/routes.py
+++ auth_backend/app/routes.py
@@ -5,13 +5,18 @@ from flask import (
 )
 from .schemas import validate_user
 from .models import User, db
+from flask_jwt_extended import (
+    JWTManager, jwt_required, create_access_token,
+    jwt_refresh_token_required, create_refresh_token,
+    get_jwt_identity, set_access_cookies,
+    set_refresh_cookies, unset_jwt_cookies,
+    get_csrf_token, fresh_jwt_required
+)
+
+jwt = JWTManager()
 
 class Register(MethodView):
     ''' endpoint to register users '''
-
-    def get(self):
-        ''' get request not allowed '''
-        return jsonify({'ok': False, 'message': "forbidden"}), 403
 
     def post(self):
         '''
@@ -38,13 +43,136 @@ class Register(MethodView):
             return jsonify({'ok': True, 'message': "user created"}), 200
 
         else:
-            return jsonify({'ok': False, 'message': "internal error"}), 400
+            return jsonify({'ok': False, 'message': "credentials do not meet specifications"}), 400
 
+
+class Login(MethodView):
+    ''' Login endpoint '''
+
+    def post(self):
+        '''
+        get the user credentials from the request
+        verify the credentials, generate the access and refresh token,
+        set the tokens in the cookies
+        return the response
+        '''
+
+        data = validate_user(request.get_json())
+        
+        if data['ok']:
+
+            user_data = data['user_data']
+
+            user = User.query.filter_by(email=user_data['email']).first()
+
+            # verify the user credentials
+            if user is None or not user.verify_password(user_data['password']):
+                return jsonify({"ok": False, "message": "Invalid Credentials"}), 401
+
+            del user_data['password']
+
+            access_token = create_access_token(identity=user_data, fresh=True)
+            refresh_token = create_refresh_token(identity=user_data)
+
+            # set the tokens in cookies
+            response = jsonify({
+                "ok": True,
+                "message": "Login Successful",
+                "access_csrf": get_csrf_token(access_token),
+                "refresh_csrf": get_csrf_token(refresh_token)
+            })
+            set_access_cookies(response, access_token)
+            set_refresh_cookies(response, refresh_token)
+
+            return response, 200
+
+        else:
+            return jsonify({"ok": False, "message": "Invalid Credentials"}), 401
+
+class FreshLogin(MethodView):
+    ''' Fresh Login endpoint '''
+
+    def post(self):
+        '''
+        get the user credentials from the request
+        verify the credentials, generate the access token,
+        set the token in the cookies
+        return the response
+        '''
+
+        data = validate_user(request.get_json())
+        
+        if data['ok']:
+
+            user_data = data['user_data']
+
+            user = User.query.filter_by(email=user_data['email']).first()
+
+            # verify the user credentials
+            if user is None or not user.verify_password(user_data['password']):
+                return jsonify({"ok": False, "message": "Invalid Credentials"}), 401
+
+            del user_data['password']
+
+            access_token = create_access_token(identity=user_data, fresh=True)
+
+            # set the tokens in cookies
+            response = jsonify({
+                "ok": True,
+                "message": "Login Successful",
+                "access_csrf": get_csrf_token(access_token),
+            })
+            set_access_cookies(response, access_token)
+
+            return response, 200
+
+        else:
+            return jsonify({"ok": False, "message": "Invalid Credentials"}), 401
+
+class RefreshToken(MethodView):
+    ''' Token refresh endpoint '''
+
+    @jwt_refresh_token_required
+    def post(self):
+        current_user = get_jwt_identity()
+        access_token = create_access_token(identity=current_user, fresh=False)
+
+        response = jsonify({
+            "ok": True,
+            "access_csrf": get_csrf_token(access_token)
+        })
+        set_access_cookies(response, access_token)
+        return response, 200
+
+class Logout(MethodView):
+    ''' Logout endpoint '''
+
+    def post(self):
+        '''
+        Remove the tokens from the cookies
+        '''
+        response = jsonify({"ok": True, "message": "Logout successful"})
+        unset_jwt_cookies(response)
+        return response, 200
+
+class ValidateToken(MethodView):
+
+    @jwt_required
+    def get(self):
+        user_data = get_jwt_identity()
+        return jsonify({"ok": True, "logged_in_as": user_data['email']}), 200
+
+class ValidateFreshToken(MethodView):
+
+    @fresh_jwt_required
+    def get(self):
+        user_data = get_jwt_identity()
+        return jsonify({"ok": True, "logged_in_as": user_data['email']}), 200
 
 class GetUser(MethodView):
-    def get(self, username):
-        user = User.query.filter_by(email=username).first()
-        if user is not None:
-            return jsonify({'user': user.email}), 200
+    def get(self):
+        user = User.query.all()
+        if user:
+            return jsonify({'user': user[0].email}), 200
         else:
             return jsonify({'ok': False, 'messsage': 'User not found'}), 404
